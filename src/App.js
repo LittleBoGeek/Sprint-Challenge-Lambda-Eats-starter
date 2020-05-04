2dd9f351e7707fa609f2699037a1fee7d75007dc
LittleBoGeek <NadiaChilds@outlook.com>
2020-05-03 23:38:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import Form from './Form'
+
 
 const App = () => {
+  
   return (
-    <>
+    <div>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <div className="header">Header</div>
+      <Link to="/pizza"> Order Pizza </Link>
+      <Route exact path="/pizza" component={Form}/>
+    </div>
   );
 };
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,140 @@
+import React, {useState, useEffect} from "react";
+import {withFormik, Form, Field} from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+const MyForm = ({errors, status,touched, values}) => {
+    const[users, setUsers] = useState([]);
+    useEffect(() => {
+        console.log("status has changed", status);
+        status && setUsers(users => [...users, status])
+
+    }, [status]);
+    return (
+        <div className ="formcont">
+
+<Form>
+
+    <label htmlFor = "name">
+        Name
+        <Field
+        id = "name"
+        type = "text"
+        name = "name"
+        placeholder =  "Name"
+        
+        />
+        {touched.name && errors.name && (
+            <p> {errors.name}</p>
+        )}
+    </label>
+
+    <select>
+  <option value="grapefruit">small</option>
+  <option value="lime">medium</option>
+  <option selected value="coconut">large</option>
+  <option value="mango">xl</option>
+</select>
+
+    <label className="checkbox-container">
+        Pepperoni
+        <Field
+          type="checkbox"
+          name="tos"
+          checked={values.tos}
+        />
+        <span className="checkmark" />
+      </label>  
+      <label className="checkbox-container">
+        Anchovies
+        <Field
+          type="checkbox"
+          name="tos"
+          checked={values.tos}
+        />
+        <span className="checkmark" />
+      </label>  
+    <label className="checkbox-container">
+        Pineapple 
+        <Field
+          type="checkbox"
+          name="tos"
+          checked={values.tos}
+        />
+        <span className="checkmark" />
+      </label>  
+   
+<label className="checkbox-container">
+       Ham
+        <Field
+          type="checkbox"
+          name="tos"
+          checked={values.tos}
+        />
+        <span className="checkmark" />
+      </label>
+      <label htmlFor = "specialInstructions">
+        Name
+        <Field
+        id = "name"
+        type = "text"
+        name = "specIn"
+        placeholder =  "Please add any special instructions here"
+        
+        />
+        {touched.name && errors.name && (
+            <p> {errors.name}</p>
+        )}
+    </label>
+  
+        <button type = "submit">Submit</button>
+
+</Form>
+{users.map(user =>{
+    return(
+    <ul key = {user.id}>
+        <li> name:{user.name}</li>
+        
+
+    </ul>
+    );
+})}
+</div>
+    );
+};
+
+
+const FormikMyForm = withFormik({
+    mapPropsToValues(props){
+
+        return{
+            name:props.name || "",
+            tos:props.tos || true,
+
+        }
+    },
+   
+
+validationSchema: Yup.object().shape({
+    name: Yup.string().required()
+   // .matches(/^[0-9]+$/, "Must be only digits")
+     .min(2, 'Must be at least two characters')
+
+  }),
+  handleSubmit(values, { setStatus, resetForm }) {
+    console.log("submitting", values);
+    axios
+      .post("https://reqres.in/api/users/", values)
+      .then(res => {
+        console.log("success", res);
+        setStatus(res.data);
+        resetForm();
+      })
+      .catch(err => console.log(err.response));
+  }
+})(MyForm);
+
+export default FormikMyForm;  
+
+
+
